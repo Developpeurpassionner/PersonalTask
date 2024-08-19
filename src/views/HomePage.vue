90832d7a966cd097379497065b530e827c1d4e46
Yékini ibrahim <97056077+Developpeurpassionner@users.noreply.github.com>
2024-08-20 01:37:02 +0200

    mon troisieme push

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -12,12 +12,12 @@
           <ion-title size="large"></ion-title>
         </ion-toolbar>
       </ion-header>
-      <div class="ion-justify-content-center">
-        <div style="margin: auto; width: 35%"><img src="../../public/notes-concept-illustration_114360-689.avif" alt=""></div>
-        <p class="ion-text-center">PersonalTask est une application qui vous permet de gérer vos taches personnelles</p>
-        <div style="display: flex;">
-            <button></button>
-            <button></button>
+      <div class="mx-auto mt-5 mb-5">
+        <div><img src="../../public/notes-concept-illustration_114360-689.avif" alt=""  class="w-96 mx-auto"></div>
+        <p class="text-center text-lg">PersonalTask est une application qui vous permet de gérer vos taches personnellement</p>
+        <div class="flex justify-center mt-8 mx-auto">
+            <button class="rounded ml-4 pt-1 pb-1 pl-2 pr-2 text-lg bg-[#0054e9] text-white w-32">Commencer</button>
+            <button class="rounded ml-16 text-lg mr-8 pt-1 pb-1 pl-8 pr-8 bg-teal-500 text-white w-32">Démo</button>
         </div>
       </div>
     </ion-content>
