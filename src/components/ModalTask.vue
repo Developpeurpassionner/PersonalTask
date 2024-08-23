f30a517956e5b8bca198f761f34524d39488f7d3
Yékini ibrahim <97056077+Developpeurpassionner@users.noreply.github.com>
2024-08-23 20:20:26 +0200

    mon cinquieme push

--- src/components/ModalTask.vue
+++ src/components/ModalTask.vue
@@ -5,17 +5,20 @@
         <i class="fa-solid fa-rectangle-xmark" style="color: black; background: yellow; z-index: 10;"></i>
         <img src="../../public/6845147.png" alt="" class="w-full">
         <div class="text-center">
-          <h1 class="block text-2xl font-bold text-white dark:text-white">Comment voulez-vous appeler cette tâche?</h1>
+          <h1 class="block text-2xl font-bold text-white dark:text-white lg:text-2xl md:text-3xl">Comment voulez-vous nommer cette tâche?</h1>
         </div>
         <div class="mt-5">
           <form>
             <div class="grid gap-y-4">
               <div>
                 <div class="relative">
-                  <input type="email" id="email" name="email" class="py-3 px-4 block w-full border-2 border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 shadow-sm" required aria-describedby="email-error">
+                  <input type="text" id="text" name="text" class="py-3 px-4 block w-full border-2 border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 shadow-sm" required>
                 </div>
               </div>
-              <button type="submit" class="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800">Valider</button>
+              <div class="flex flex-col mx-auto justify-between space-y-8 mt-6 lg:mt-0 md:space-y-10 lg:flex-row lg:mx-0 lg:space-y-0 md:flex-col">
+                <button class="py-1 lg:py-0 px-8 text-2xl lg:text-lg md:text-4xl inline-flex justify-center items-center w-34 lg:w-32 gap-2 rounded-md border border-transparent  bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all dark:focus:ring-offset-gray-800">Valider</button>
+                <button class="hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2 transition-all dark:focus:ring-offset-gray-800 rounded-md text-2xl py-1 px-8 bg-teal-500 text-white w-34  md:w-52 lg:w-32 lg:py-0 md:text-4xl lg:text-lg">Annuler</button>
+              </div>
             </div>
           </form>
         </div>
