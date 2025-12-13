86bfaf9f1bcf30565643f1f8c6bb9e9da21a8822
Yékini ibrahim <97056077+Developpeurpassionner@users.noreply.github.com>
2025-12-13 08:18:07 +0100

    Modification du fichier capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.ibrahim.personaltask',
   appName: 'PersonnalTask',
-  webDir: 'public'
+  webDir: 'dist'
 };
 
 export default config;
